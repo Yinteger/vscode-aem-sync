02b965e1a3edc8bff7e29d493d99eb4fb8922679
Yinkai <kyle.williams@alc.ca>
2018-08-17 17:13:38 -0300

    Quickfix for a path issue with unix

--- src/packager.js
+++ src/packager.js
@@ -64,7 +64,12 @@ class Packager {
     }
     //Clean a system path and convert to AEM Path
     convertPathToAem(path) {
-        return "/" + path.split('jcr_root\\')[1].replace(/\\/g, "/");
+        if (path.indexOf("jcr_root\\") > -1) {
+            return "/" + path.split('jcr_root\\')[1].replace(/\\/g, "/");
+        } else if (path.indexOf("jcr_root/") > -1) {
+            return "/" + path.split("jcr_root/")[1].replace(/\\/g, "/");
+        }
+        
     }
     //Clear the package file from the system
     clearPackage () {
